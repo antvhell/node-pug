2a47031a6766451e30c1e6379579bdf2a1203659
tOñO <antvhell@hotmail.com>
2024-04-18 16:42:40 -0600

    Pagina de crear cuenta

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,13 @@
 const formularioLogin = (req, res) => {
-  res.render("auth/login", {});
+  res.render("auth/login", {
+    pagina: "Iniciar Sesión",
+  });
 };
 
 const formularioRegistro = (req, res) => {
-  res.render("auth/registro", {});
+  res.render("auth/registro", {
+    pagina: "Crear Cuenta",
+  });
 };
 
 export { formularioLogin, formularioRegistro };
