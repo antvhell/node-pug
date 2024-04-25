6528f7428354b9faccc56f6637f2f313064adc6d
tOñO <antvhell@hotmail.com>
2024-04-25 15:48:59 -0600

    Implementando autenticacion de usuarios

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import express from "express";
+import csrf from "csurf";
 import usuarioRoutes from "./routes/usuariosRoutes.js";
+import db from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 //Crear la app
 const app = express();
+
+// Habilitar lectura de datos de formulario
+app.use(express.urlencoded({ extended: true }));
+
+// Habilitar Cookie Parser
+app.use(cookieParser());
+
+// Habilitar CSRF
+app.use(csrf({ cookie: true }));
+
+// Conexion a la base de datos
+try {
+  await db.authenticate();
+  db.sync();
+  console.log("Conexion correcta a la Base de datos");
+} catch (error) {
+  console.log(error);
+}
 
 // Routing
 app.use("/auth", usuarioRoutes);
@@ -15,7 +36,7 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 // Definir un puerto y arrancar el proyecto
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,9 +1,31 @@
 import express from "express";
-import { formularioLogin, formularioRegistro } from "../controllers/usuarioController.js";
+import {
+  formularioLogin,
+  autenticar,
+  formularioRegistro,
+  registrar,
+  formulario0lvidePassword,
+  confirmar,
+  resetPassword,
+  comprobarToken,
+  nuevoPassword,
+} from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 router.get("/login", formularioLogin);
+router.post("/login", autenticar);
+
 router.get("/registro", formularioRegistro);
+router.post("/registro", registrar);
+
+router.get("/confirmar/:token", confirmar);
+
+router.get("/olvide-password", formulario0lvidePassword);
+router.post("/olvide-password", resetPassword);
+
+// Almacena el nuevo password
+router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword);
 
 export default router;
